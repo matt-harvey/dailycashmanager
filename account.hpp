563c404e69f69212dd7c8bd1e0119c32a36a6eb2
Matthew Harvey <software@matthewharvey.net>
2012-06-24 03:08:51 +0000

    Deleted setters and getters from Phatbooks classes. These should only be provided if and when required.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@103 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- account.hpp
+++ account.hpp
@@ -14,13 +14,6 @@ public:
 	// initially let's use cents. We can make it more sophisticated
 	// later
 	typedef int BalanceType;
-	BalanceType balance() const;
-	Commodity commodity() const;
-	std::string name() const;
-	std::string description() const;
-	void set_balance(BalanceType);
-	void set_name(std::string const&);
-	void set_description(std::string const&);
 private:
 	Commodity commodity_;
 	BalanceType balance_;
@@ -29,46 +22,6 @@ private:
 };
 
 // inline member functions
-
-
-inline Account::BalanceType Account::balance() const
-{
-	return balance_;
-}
-
-inline Commodity Account::commodity() const
-{
-	return commodity_;
-}
-
-inline std::string Account::name() const
-{
-	return name_;
-}
-
-inline std::string Account::description() const
-{
-	return description_;
-}
-
-inline void Account::set_balance(Account::BalanceType new_balance)
-{
-	balance_ = new_balance;
-	return;
-}
-
-inline void Account::set_name(std::string const& new_name)
-{
-	name_ = new_name;
-	return;
-}
-
-inline void Account::set_description(std::string const& new_description)
-{
-	description_ = new_description;
-	return;
-}
-
 
 
 
--- commodity.hpp
+++ commodity.hpp
@@ -6,17 +6,12 @@
 class Commodity
 {
 public:
-	std::string str_rep() const;
 private:
 	std::string str_rep_;
 };
 
 // inline member functions
 
-inline std::string Commodity::str_rep() const
-{
-	return str_rep_;
-}
 
 
 #endif
