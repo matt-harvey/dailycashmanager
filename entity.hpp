7701d72c42af644a21001a9be2f92fb9ca232fb0
Matthew Harvey <software@matthewharvey.net>
2012-07-08 07:04:28 +0000

    Added Entity class.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@123 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

---
+++ entity.hpp
@@ -0,0 +1,24 @@
+#ifndef GUARD_entity_hpp
+#define GUARD_entity_hpp
+
+#include "database.hpp"
+#include <string>
+
+
+namespace phatbooks
+{
+
+class Entity
+{
+public:
+protected:
+private:
+	Database m_database;
+	std::string m_name;
+	IdType commodity_id;
+};
+
+
+}  // namespace phatbooks
+
+#endif
