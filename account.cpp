03911c1c4e4bc5ca29ae971c792036da6555525d
Matthew Harvey <software@matthewharvey.net>
2012-07-21 19:25:28 +0000

    Added account.cpp
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@158 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

---
+++ account.cpp
@@ -0,0 +1,35 @@
+#include "account.hpp"
+#include <string>
+
+using std::string;
+
+namespace phatbooks
+{
+
+Account::Account(AccountType p_account_type, string p_name,
+  string p_description):
+  m_account_type(p_account_type),
+  m_name(p_name),
+  m_description(p_description)
+{
+}
+ 
+Account::AccountType
+Account::account_type() const
+{
+	return m_account_type;
+}
+
+string Account::name() const
+{
+	return m_name;
+}
+
+string Account::description() const
+{
+	return m_description;
+}
+
+
+
+}  // namespace phatbooks
