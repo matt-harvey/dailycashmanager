09e91a5fa1e30b47f118befbd69d0f0fdb89f795
Matthew Harvey <software@matthewharvey.net>
2012-09-16 08:40:46 +0000

    Removed unused store methods from PhatbooksDatabaseConnection.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@352 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- phatbooks_database_connection/phatbooks_database_connection.hpp
+++ phatbooks_database_connection/phatbooks_database_connection.hpp
@@ -29,21 +29,6 @@ class Journal;
 /** Phatbooks-specific database connection class.
  * See API documentation for sqloxx::DatabaseConnection,
  * for parts of API inherited from sqloxx::DatabaseConnection.
- *
- * @todo Factor out code common to different \c store functions.
- *
- * @todo If speed becomes a problem, I should find a way to avoid
- * having to call sqlite3_prepare_v2, by caching previously prepared
- * sqlite3_stmt* somehow. I think the only feasible way would be to store it
- * as a static member
- * variable of DatabaseConnection. That way the sqlite3_stmt can
- * be freed using sqlite3_finalize in the destructor.
- * There would end up being several of the pre-prepared statements.
- * I tried making the prepared SQL statement a static variable in the
- * \c function, but this meant I couldn't call sqlite3_finalize on the
- * statement at the appropriate time.
- * Having said this, there's no point unless
- * it causes a problem by being too slow.
  */
 class PhatbooksDatabaseConnection:
 	public sqloxx::DatabaseConnection
@@ -54,58 +39,6 @@ public:
 	 * default constructor for sqloxx::DatabaseConnection.
 	 */
 	PhatbooksDatabaseConnection();
-	
-	/** Store an Account object in the database
-	 * 
-	 * @todo Verify that throwing behaviour is as documented.
-	 * 
-	 * @throws StoragePreconditionsException if p_account has invalid
-	 * commodity abbreviation. (Commodity abbreviation of
-	 * p_account must correspond to a commodity that has already
-	 * been stored in the database.)
-	 *
-	 * @throws sqloxx::NoPrimaryKeyException if the table for storing
-	 * Account data does not have a primary key.
-	 *
-	 * @throws sqloxx::CompoundPrimaryKeyException if the table for storing
-	 * Account data has a compound primary key.
-	 * 
-	 * @throws sqloxx::TableSizeException if the table has reached its
-	 * maximum size and therefore cannot accept any additional rows.
-	 *
-	 * @throws sqloxx:DatabaseExceptions or some derivative thereof, if
-	 * something else goes wrong in finding the primary key to be
-	 * assigned to the inserted object. (This is not expected ever to occur
-	 * unless there is a heap allocation issue or the database is corrupt.)
-	 *
-	 * @throws sqloxx::InvalidConnection if the database connection is invalid
-	 * at the time the storage is attempted.
-	 *
-	 * @throws sqloxx::SQLiteException or some derivative thereof, if there
-	 * is some error binding the data for \c p_account to the SQL statement
-	 * involved in storing the account, or in executing the
-	 * resulting SQL statement. This not expected to occur except
-	 * in cases of a corrupt database or memory allocation failure.
-	 *
-	 * @throws sqloxx::ConstraintException if there are multiple commodities
-	 * with the same commodity abbreviation as the commodity abbreviation
-	 * of \c p_account (It is not expected this will occur except case of
-	 * a corrupt database, or a bug in Phatbooks.)
-	 *
-	 * @param p_account the Account to be stored.
-	 *
-	 * @returns the integer primary key of the Account just stored.
-	 * (This is an autoincrementing primary key.)
-	 */
-	IdType store(Account const& p_account);
-
-	/**
-	 * @todo Figure out and document throwing behaviour.
-	 *
-	 * @todo Resolve issue that this does not take draft journals
-	 * into account.
-	 */
-	IdType store(Journal const& p_journal);
 
 	/**
 	 * @returns \c true if and only if \c p_name is the name of an Account
