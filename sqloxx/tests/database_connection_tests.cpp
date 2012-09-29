295e26edbb5423ec5d259aa4c418073bc11a80a4
Matthew Harvey <software@matthewharvey.net>
2012-09-29 08:46:10 +0000

    Continued work on unit testing of DatabaseConnection.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@408 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- sqloxx/tests/database_connection_tests.cpp
+++ sqloxx/tests/database_connection_tests.cpp
@@ -4,11 +4,13 @@
 #include <boost/filesystem.hpp>
 #include <cassert>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
 
 using std::cerr;
 using std::endl;
+using std::ofstream;
 using std::string;
 
 
@@ -33,6 +35,24 @@ namespace
 		}
 		return;
 	}
+
+	bool file_exists(boost::filesystem::path const& filepath)
+	{
+		return boost::filesystem::exists
+		(	boost::filesystem::status(filepath)
+		);
+	}
+
+	void abort_if_exists(boost::filesystem::path const& filepath)
+	{
+		if (file_exists(filepath))
+		{
+			cerr << "File named \"" << filepath.string() << "\" already "
+			     << "exists. Test aborted." << endl;
+			std::abort();
+		}
+	}
+			    
 }  // End anonymous namespace
 
 
@@ -41,23 +61,60 @@ namespace
 // database file is created and then destroyed in this test.
 TEST(test_default_constructor_and_open)
 {
-	string const filename("Testfile_00");	
-	boost::filesystem::path const filepath(filename);
-	if (boost::filesystem::exists(boost::filesystem::status(filepath)))
-	{
-		cerr << "File named \"" << filename << "\" already exists. "
-		     << "Test aborted." << endl;
-		std::abort();
-	}
+	// Define filepaths (without opening)
+	boost::filesystem::path const filepath("T55555l2_009873");
+	abort_if_exists(filepath);
+	boost::filesystem::path const dummy_filepath("lasdy98989823");
+	abort_if_exists(dummy_filepath);
+	boost::filesystem::path const another_filepath("th887kk899872");
+	abort_if_exists(another_filepath);
+	boost::filesystem::path const empty_filepath("");
+	abort_if_exists(empty_filepath);
+
+	// Test opening new file
 	DatabaseConnection dbc;
 	CHECK(!dbc.is_valid());
-	dbc.open(filename);
+	dbc.open(filepath);  // Note passing filename would also have worked
 	CHECK(dbc.is_valid());
-	CHECK(boost::filesystem::exists(boost::filesystem::status(filepath)));
+	CHECK(file_exists(filepath));
 	catch_check_ok(dbc);
+
+	// Test behaviour when calling open on an existing connection
+	CHECK_THROW(dbc.open(filepath), MultipleConnectionException);
+	CHECK(file_exists(filepath));
+	catch_check_ok(dbc);
+
+	// Test behaviour when calling open with some other filename corresponding
+	// to a file that does not exist.
+	CHECK_THROW(dbc.open(dummy_filepath), MultipleConnectionException);
+	CHECK(!file_exists(dummy_filepath));
+	CHECK(file_exists(filepath));
+	catch_check_ok(dbc);
+
+	// Test behaviour when calling open with some other filename corresponding
+	// to a file that does exist, but is not connected to with any database
+	// connection
+	assert (!file_exists(another_filepath));
+	ofstream ifs(another_filepath.string().c_str());
+	assert (file_exists(another_filepath));
+	CHECK_THROW
+	(	dbc.open(another_filepath), MultipleConnectionException
+	);
+	boost::filesystem::remove(another_filepath);
+	catch_check_ok(dbc);
+
+	// Test opening with an empty string
+	DatabaseConnection dbc2;
+	CHECK(!dbc2.is_valid());
+	CHECK_THROW(dbc2.open(empty_filepath), InvalidFilename);
+	CHECK(!file_exists(empty_filepath));
+
+	// Cleanup
 	boost::filesystem::remove(filepath);
 	assert (!boost::filesystem::exists(boost::filesystem::status(filepath)));
+	assert (!file_exists(filepath));
 }	
+
 
 
 // Fixture that creates a DatabaseConnection and database file for
