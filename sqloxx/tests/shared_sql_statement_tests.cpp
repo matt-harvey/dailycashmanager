acc79c4f3e1d2cba0d5255fbacc14162437516ea
Matthew Harvey <software@matthewharvey.net>
2012-10-05 09:55:49 +0000

    Continued work on unit tests for sqloxx::SharedSQLStatement.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@424 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- sqloxx/tests/shared_sql_statement_tests.cpp
+++ sqloxx/tests/shared_sql_statement_tests.cpp
@@ -4,6 +4,10 @@
 #include "sqloxx/tests/sqloxx_tests_common.hpp"
 
 #include <unittest++/UnitTest++.h>
+#include <boost/cstdint.hpp>
+#include <string>
+
+using std::string;
 
 namespace sqloxx
 {
@@ -28,10 +32,55 @@ TEST_FIXTURE(DatabaseConnectionFixture, test_shared_sql_statement_constructor)
 		),
 		InvalidConnection
 	);
+
+	// Now let's create an unproblematic SQLStatement. This shouldn't throw.
+	SharedSQLStatement normal(dbc, "create table dummy(Col_A text)");
 }
 
-// WARNING INCOMPLETE TESTS
 
+TEST_FIXTURE(DatabaseConnectionFixture, test_bind_and_extract_normal)
+{
+	dbc.execute_sql
+	(	"create table dummy(Col_A integer primary key autoincrement, "
+		"Col_B text not null, Col_C integer not null, Col_D integer)"
+	);
+
+	SharedSQLStatement statement_01
+	(	dbc,
+		"insert into dummy(Col_B, Col_C, Col_D) values(:B, :C, :D)"
+	);
+	string const hello_01("hello");
+	int const x_01(30);
+	boost::int64_t y_01(999999983);
+	statement_01.bind(":B", hello_01);
+	statement_01.bind(":C", x_01);
+	statement_01.bind(":D", y_01);
+	statement_01.step_final();
+
+	string const goodbye_02("goodbye");
+	int const x_02(293874);
+	SharedSQLStatement statement_02
+	(	dbc,
+		"insert into dummy(Col_B, Col_C) values(:B, :C)"
+	);
+	statement_02.bind(":B", goodbye_02);
+	statement_02.bind(":C", x_02);
+	statement_02.step_final();
+
+	SharedSQLStatement selector
+	(	dbc,
+		"select Col_B, Col_C, Col_D from dummy where Col_A = 2"
+	);
+	selector.step();
+	CHECK_EQUAL(selector.extract<string>(0), goodbye_02);
+	CHECK_EQUAL(selector.extract<int>(1), x_02);
+	selector.step_final();
+}
+
+
+// WARNING STILL NEED TO TEST BIND AND EXTRACT EXCEPTIONS
+
+// WARNING STILL NEED TO TEST REST OF API
 
 
 
