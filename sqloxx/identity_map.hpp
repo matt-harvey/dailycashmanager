79a605631b702dc205ebd2ad15f8edf084116b73
Matthew Harvey <software@matthewharvey.net>
2012-11-25 12:03:34 +0000

    Tidied sqloxx/identity_map.hpp.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@660 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- sqloxx/identity_map.hpp
+++ sqloxx/identity_map.hpp
@@ -24,13 +24,10 @@ class IdentityMap
 {
 public:
 
-	IdentityMap(Connection& p_connection):
-		m_map_data(new MapData(p_connection))
-	{
-	}
-
 	typedef typename T::Id Id;
-	typedef Id ProxyKey;
+	typedef typename T::Id ProxyKey;
+
+	IdentityMap(Connection& p_connection);
 
 	/**
 	 * Provide handle to object of T, representing a newly created object
@@ -46,61 +43,24 @@ public:
 	
 	/**
 	 * Register id of newly saved T.
-	 *
-	 * WARNING Move implementation out of class body.
 	 */
-	void register_id(ProxyKey proxy_key, Id allocated_id)
-	{
-		m_map_data->id_map[allocated_id] = m_map_data->proxy_map[proxy_key];
-		return;
-	}
+	void register_id(ProxyKey proxy_key, Id allocated_id);
 
-	void erase_object_proxied(ProxyKey proxy_key)
-	{
-		Record const record = m_map_data->proxy_map.find(proxy_key)->second;
-		if (record->has_id())
-		{
-			assert (m_map_data->id_map.find(record->id()) != m_map_data->id_map.end());
-			m_map_data->id_map.erase(record->id());
-		}
-		m_map_data->proxy_map.erase(proxy_key);
-		return;
-	}
-
+	void erase_object_proxied(ProxyKey proxy_key);
 
 	/**
 	 * Notify the IdentityMap that there are no handles left that are
 	 * pointing to this object.
-	 *
-	 * WARNING Move implementation out of class body. 
 	 */
-	void notify_nil_handles(ProxyKey proxy_key)
-	{
-		if (!m_map_data->caching)
-		{
-			erase_object_proxied(proxy_key);
-		}
-		return;
-	}
+	void notify_nil_handles(ProxyKey proxy_key);
 
-
-	void enable_caching()
-	{
-		m_map_data->caching = true;
-	}
+	void enable_caching();
 
 	void disable_caching();
 
-	Connection& connection()
-	{
-		return m_map_data->connection;
-	}
+	Connection& connection();
 
-	IdentityMap& operator=(IdentityMap const& rhs)
-	{
-		m_map_data = rhs.m_map_data;
-		return *this;
-	}
+	IdentityMap& operator=(IdentityMap const& rhs);
 
 private:
 
@@ -111,6 +71,20 @@ private:
 	typedef boost::unordered_map<Id, Record> IdMap;
 	typedef std::map<ProxyKey, Record> ProxyKeyMap;
 
+	ProxyKeyMap& proxy_map() const
+	{
+		return m_map_data->proxy_map;
+	}
+
+	IdMap& id_map() const
+	{
+		return m_map_data->id_map;
+	}
+
+	bool& caching() const
+	{
+		return m_map_data->caching;
+	}
 
 	// Data members
 
@@ -136,6 +110,12 @@ private:
 
 
 template <typename T, typename Connection>
+IdentityMap<T, Connection>::IdentityMap(Connection& p_connection):
+	m_map_data(new MapData(p_connection))
+{
+}
+
+template <typename T, typename Connection>
 Handle<T>
 IdentityMap<T, Connection>::provide_object()
 {
@@ -143,7 +123,7 @@ IdentityMap<T, Connection>::provide_object()
 	Record obj_ptr((new T(*this)));
 	ProxyKey const proxy_key = next_proxy_key();
 	obj_ptr->set_proxy_key(proxy_key);
-	m_map_data->proxy_map[proxy_key] = obj_ptr;
+	proxy_map()[proxy_key] = obj_ptr;
 	return Handle<T>(obj_ptr);
 }
 
@@ -152,30 +132,72 @@ template <typename T, typename Connection>
 Handle<T>
 IdentityMap<T, Connection>::provide_object(Id p_id)
 {
-	typename IdMap::iterator it = m_map_data->id_map.find(p_id);
-	if (it == m_map_data->id_map.end())
+	typename IdMap::iterator it = id_map().find(p_id);
+	if (it == id_map().end())
 	{
 		// Then we need to create this object.
 		Record obj_ptr(new T(*this, p_id));
-		m_map_data->id_map[p_id] = obj_ptr;
+		id_map()[p_id] = obj_ptr;
 		ProxyKey proxy_key = next_proxy_key();
-		m_map_data->proxy_map[proxy_key] = obj_ptr;
+		proxy_map()[proxy_key] = obj_ptr;
 		obj_ptr->set_proxy_key(proxy_key);
 		return Handle<T>(obj_ptr); 
 	}
-	assert (it != m_map_data->id_map.end());
+	assert (it != id_map().end());
 	return Handle<T>(it->second);
+}
+
+template <typename T, typename Connection>
+void
+IdentityMap<T, Connection>::register_id(ProxyKey proxy_key, Id allocated_id)
+{
+	id_map()[allocated_id] = proxy_map()[proxy_key];
+	return;
+}
+
+
+template <typename T, typename Connection>
+void
+IdentityMap<T, Connection>::erase_object_proxied(ProxyKey proxy_key)
+{
+	Record const record = proxy_map().find(proxy_key)->second;
+	if (record->has_id())
+	{
+		assert (id_map().find(record->id()) != id_map().end());
+		id_map().erase(record->id());
+	}
+	proxy_map().erase(proxy_key);
+	return;
+}
+
+template <typename T, typename Connection>
+void
+IdentityMap<T, Connection>::notify_nil_handles(ProxyKey proxy_key)
+{
+	if (!caching())
+	{
+		erase_object_proxied(proxy_key);
+	}
+	return;
+}
+
+template <typename T, typename Connection>
+void
+IdentityMap<T, Connection>::enable_caching()
+{
+	caching() = true;
 }
 
 template <typename T, typename Connection>
 void
 IdentityMap<T, Connection>::disable_caching()
 {
-	if (m_map_data->caching)
+	if (caching())
 	{
+		typename ProxyKeyMap::iterator const endpoint = proxy_map().end();
 		for
-		(	typename ProxyKeyMap::iterator it = m_map_data->proxy_map.begin();
-			it != m_map_data->proxy_map.end();
+		(	typename ProxyKeyMap::iterator it = proxy_map().begin();
+			it != endpoint;
 			++it
 		)
 		{
@@ -184,11 +206,26 @@ IdentityMap<T, Connection>::disable_caching()
 				erase_object_proxied(it->first);
 			}
 		}
-		m_map_data->caching = false;
+		caching() = false;
 	}
 	return;
 }
-	
+
+template <typename T, typename Connection>
+Connection&
+IdentityMap<T, Connection>::connection()
+{
+	return m_map_data->connection;
+}
+
+template <typename T, typename Connection>
+IdentityMap<T, Connection>&
+IdentityMap<T, Connection>::operator=(IdentityMap const& rhs)
+{
+	m_map_data = rhs.m_map_data;
+	return *this;
+}
+
 template <typename T, typename Connection>
 typename IdentityMap<T, Connection>::ProxyKey
 IdentityMap<T, Connection>::next_proxy_key()
@@ -202,15 +239,13 @@ IdentityMap<T, Connection>::next_proxy_key()
 	// with Id.
 	// Relies on this being a std::map, in which the first
 	// key is less than any other key.
-	ProxyKey const least = m_map_data->proxy_map.begin()->first;
+	ProxyKey const least = proxy_map().begin()->first;
 	if (least == std::numeric_limits<ProxyKey>::min())
 	{
 		throw OverflowException("Proxy key has reached numeric limit.");
 	}
 	return least - 1;
 }
-
-	
 
 }  // namespace sqloxx
 
