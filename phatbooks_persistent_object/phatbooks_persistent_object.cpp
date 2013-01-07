659ae9728360e2ecca645d6bcc542b18920d41f0
Matthew Harvey <software@matthewharvey.net>
2013-01-07 21:18:04 +0000

    Actually added phatbooks_persistent_object.cpp to version control.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@943 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

---
+++ phatbooks_persistent_object/phatbooks_persistent_object.cpp
@@ -0,0 +1,52 @@
+#include "phatbooks_persistent_object.hpp"
+#include "phatbooks_database_connection.hpp"
+
+namespace phatbooks
+{
+
+void
+PhatbooksPersistentObjectBase::save()
+{
+	do_save();
+	return;
+}
+
+PhatbooksPersistentObjectBase::Id
+PhatbooksPersistentObjectBase::id() const
+{
+	return do_get_id();
+}
+
+PhatbooksDatabaseConnection&
+PhatbooksPersistentObjectBase::database_connection() const
+{
+	return do_get_database_connection();
+}
+
+bool
+PhatbooksPersistentObjectBase::has_id() const
+{
+	return does_have_id();
+}
+
+void
+PhatbooksPersistentObjectBase::remove()
+{
+	do_remove();
+	return;
+}
+
+void
+PhatbooksPersistentObjectBase::ghostify()
+{
+	do_ghostify();
+	return;
+}
+
+
+
+
+
+
+
+}  // namespace phatbooks
