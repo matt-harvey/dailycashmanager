b3fba1b29c66d0bc2b3717e2eafb06bba4c2f5a6
Matthew Harvey <software@matthewharvey.net>
2013-03-14 00:32:05 +0000

    Changes code in tests for Repeater, to accommodate new Frequency-based interface to Repeater.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@1198 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- tests/repeater_tests.cpp
+++ tests/repeater_tests.cpp
@@ -2,6 +2,7 @@
 #include "b_string.hpp"
 #include "draft_journal.hpp"
 #include "entry.hpp"
+#include "frequency.hpp"
 #include "interval_type.hpp"
 #include "phatbooks_tests_common.hpp"
 #include "repeater.hpp"
@@ -47,8 +48,7 @@ TEST_FIXTURE(TestFixture, test_repeater_next_date)
 	dj.push_entry(entry2);
 
 	Repeater repeater1(dbc);
-	repeater1.set_interval_type(interval_type::days);
-	repeater1.set_interval_units(3);
+	repeater1.set_frequency(Frequency(3, interval_type::days));
 	repeater1.set_next_date(date(2012, 5, 30));
 	dj.push_repeater(repeater1);
 	dj.save();
@@ -59,8 +59,7 @@ TEST_FIXTURE(TestFixture, test_repeater_next_date)
 	CHECK_EQUAL(repeater1.next_date(1), date(2012, 6, 2));
 
 	Repeater repeater2(dbc);
-	repeater2.set_interval_type(interval_type::weeks);
-	repeater2.set_interval_units(2);
+	repeater2.set_frequency(Frequency(2, interval_type::weeks));
 	repeater2.set_next_date(date(2012, 12, 31));
 	dj.push_repeater(repeater2);
 	dj.save();
@@ -69,14 +68,12 @@ TEST_FIXTURE(TestFixture, test_repeater_next_date)
 	CHECK_EQUAL(repeater2.next_date(1), date(2013, 1, 14));
 
 	Repeater repeater3(dbc);
-	repeater3.set_interval_type(interval_type::months);
-	repeater3.set_interval_units(1);
+	Repeater3.set_frequency(Frequency(1, interval_type::months));
 	repeater3.set_next_date(date(2014, 9, 20));
 	CHECK_EQUAL(repeater3.next_date(5), date(2015, 2, 20));
 
 	Repeater repeater4(dbc);
-	repeater4.set_interval_type(interval_type::month_ends);
-	repeater4.set_interval_units(1);
+	repeater4.set_frequency(Frequency(1, interval_type::month_ends));
 	repeater4.set_next_date(date(1996, 1, 31));
 	CHECK_EQUAL(repeater4.next_date(), date(1996, 1, 31));
 	CHECK_EQUAL(repeater4.next_date(1), date(1996, 2, 29));
@@ -85,15 +82,13 @@ TEST_FIXTURE(TestFixture, test_repeater_next_date)
 	CHECK_EQUAL(repeater4.next_date(13), date(1997, 2, 28));
 
 	Repeater repeater5(dbc);
-	repeater5.set_interval_type(interval_type::days);
-	repeater5.set_interval_units(1);
+	repeater5.set_frequency(Frequency(1, interval_type::days));
 	repeater5.set_next_date(date(1900, 2, 27));
 	CHECK_EQUAL(repeater5.next_date(1), date(1900, 2, 28));
 	CHECK_EQUAL(repeater5.next_date(2), date(1900, 3, 1));
 
 	Repeater repeater6(dbc);
-	repeater6.set_interval_type(interval_type::month_ends);
-	repeater6.set_interval_units(12);
+	repeater6.set_frequency(Frequency(12, interval_type::month_ends));
 	repeater6.set_next_date(date(1999, 2, 28));
 	CHECK_EQUAL(repeater6.next_date(1), date(2000, 2, 29));
 
@@ -107,8 +102,7 @@ TEST_FIXTURE(TestFixture, test_repeater_firings_till)
 	PhatbooksDatabaseConnection& dbc = *pdbc;
 
 	Repeater repeater1(dbc);
-	repeater1.set_interval_type(interval_type::days);
-	repeater1.set_interval_units(5);
+	repeater1.set_frequency(Frequency(5, interval_type::days));
 	repeater1.set_next_date(date(2000, 5, 3));
 	shared_ptr<vector<date> > firings1 =
 		repeater1.firings_till(date(1999, 5, 2));
@@ -129,8 +123,7 @@ TEST_FIXTURE(TestFixture, test_repeater_firings_till)
 	CHECK_EQUAL((*firings1)[3], date(2000, 5, 18));
 
 	Repeater repeater2(dbc);
-	repeater2.set_interval_type(interval_type::month_ends);
-	repeater2.set_interval_units(3);
+	repeater2.set_frequency(Frequency(3, interval_type::month_ends));
 	repeater2.set_next_date(date(2012, 12, 31));
 	shared_ptr<vector<date> > firings2;
 	firings2 = repeater2.firings_till(date(2012, 12, 30));
@@ -171,8 +164,7 @@ TEST_FIXTURE(TestFixture, test_repeater_fire_next)
 	dj1.push_entry(entry1b);
 
 	Repeater repeater1(dbc);
-	repeater1.set_interval_type(interval_type::weeks);
-	repeater1.set_interval_units(2);
+	repeater1.set_frequency(Frequency(2, interval_type::weeks));
 	repeater1.set_next_date(date(2012, 7, 30));
 	dj1.push_repeater(repeater1);
 
@@ -212,53 +204,44 @@ TEST_FIXTURE(TestFixture, test_repeater_frequency_phrase)
 	PhatbooksDatabaseConnection& dbc = *pdbc;
 
 	Repeater repeater1(dbc);
-	repeater1.set_interval_type(interval_type::days);
-	repeater1.set_interval_units(1);
+	repeater1.set_frequency(Frequency(1, interval_type::days));
 	CHECK_EQUAL(frequency_description(repeater1), "every day");
 	CHECK(typeid(frequency_description(repeater1)) == typeid(std::string));
 	CHECK_EQUAL(frequency_description(repeater1), BString("every day"));
 
 	Repeater repeater2(dbc);
-	repeater2.set_interval_type(interval_type::days);
-	repeater2.set_interval_units(12);
+	repeater2.set_frequency(Frequency(12, interval_type::days));
 	CHECK_EQUAL(frequency_description(repeater2), "every 12 days");
 
 	Repeater repeater3(dbc);
-	repeater3.set_interval_type(interval_type::weeks);
-	repeater3.set_interval_units(1);
+	repeater3.set_frequency(Frequency(1, interval_type::weeks));
 	CHECK_EQUAL(frequency_description(repeater3), "every week");
 	
 	Repeater repeater4(dbc);
-	repeater4.set_interval_type(interval_type::weeks);
-	repeater4.set_interval_units(2);
+	repeater4.set_frequency(Frequency(2, interval_type::weeks));
 	CHECK_EQUAL(frequency_description(repeater4), "every 2 weeks");
 
 	Repeater repeater5(dbc);
-	repeater5.set_interval_type(interval_type::months);
-	repeater5.set_interval_units(3);
+	repeater5.set_frequency(Frequency(3, interval_type::months));
 	CHECK_EQUAL(frequency_description(repeater5), "every 3 months");
 
 	Repeater repeater6(dbc);
-	repeater6.set_interval_type(interval_type::months);
-	repeater6.set_interval_units(1);
+	repeater6.set_frequency(Frequency(1, interval_type::months));
 	CHECK_EQUAL(frequency_description(repeater6), "every month");
 
 	Repeater repeater7(dbc);
-	repeater7.set_interval_type(interval_type::months);
-	repeater7.set_interval_units(12);
+	repeater7.set_frequency(Frequency(12, interval_type::months));
 	CHECK_EQUAL(frequency_description(repeater7), "every 12 months");
 
 	Repeater repeater8(dbc);
-	repeater8.set_interval_type(interval_type::month_ends);
-	repeater8.set_interval_units(1);
+	repeater8.set_frequency(Frequency(1, interval_type::month_ends));
 	CHECK_EQUAL
 	(	frequency_description(repeater8),
 		"every month, on the last day of the month"
 	);
 
 	Repeater repeater9(dbc);
-	repeater9.set_interval_type(interval_type::month_ends);
-	repeater9.set_interval_units(10);
+	repeater9.set_frequency(Frequency(10, interval_type::month_ends));
 	CHECK_EQUAL
 	(	frequency_description(repeater9),
 		"every 10 months, on the last day of the month"
