c1591c5f7243d746f49cb0a1a058ca07b39e5dd6
Matthew Harvey <software@matthewharvey.net>
2013-04-25 00:03:57 +0000

    Continued working on AccountDataViewModel.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@1384 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- graphical_session/wxs/account_data_view_model.cpp
+++ graphical_session/wxs/account_data_view_model.cpp
@@ -1,9 +1,128 @@
 #include "account_data_view_model.hpp"
+#include "account.hpp"
+#include <boost/shared_ptr.hpp>
+#include <jewel/decimal.hpp>
+#include <wx/dataview.h>
+#include <wx/variant.h>
+#include <cassert>
+#include <vector>
+
+using boost::shared_ptr;
+using jewel::Decimal;
+using std::vector;
 
 namespace phatbooks
 {
 namespace gui
 {
+
+AccountDataViewModel::AccountDataViewModel
+(	vector<AugmentedAccount> const& p_accounts
+):
+	// Start at 1 as 0 is reserved for invalid wxDataViewItem
+	m_least_available_identifier(1)
+{
+	vector<AugmentedAccount>::size_type i = 0;
+	vector<AugmentedAccount>::size_type const max = p_accounts.size();
+	while (i != max)
+	{
+		shared_ptr<AugmentedAccount> account
+		(	new AugmentedAccount(p_accounts[i++])
+		);
+		m_accounts[m_least_available_identifier++] = account;
+		assert (i + 1 == m_least_available_identifier);
+	}	
+}
+
+bool
+AccountDataViewModel::IsContainer(wxDataViewItem const& item) const
+{
+	(void)item;  // silence compiler warning re. unused variable
+	return false;
+}
+
+wxDataViewItem
+AccountDataViewModel::GetParent(wxDataViewItem const& item) const
+{
+	(void)item;  // silence compiler warning re. unused variable
+	
+	// Always returns an invalid wxDataViewItem since none
+	// of the items have parents that are not the root
+	return wxDataViewItem(0);
+}
+
+unsigned int
+AccountDataViewModel::GetChildren
+(	wxDataViewItem const& item,
+	wxDataViewItemArray& children
+) const
+{
+	if (item == wxDataViewItem(0))
+	{
+		// Then the item is the root and all the AugmentedAccounts are its
+		// children. Deliberately start counting from 1, as 0 is for an
+		// invalid wxDataViewItem.
+		return get_all_items(children);
+	}
+	return 0;
+}
+	
+unsigned int
+AccountDataViewModel::GetColumnCount() const
+{
+	return s_num_columns;
+}
+
+wxString
+AccountDataViewModel::GetColumnType(unsigned int col) const
+{
+	switch (col)
+	{
+	case 0:
+	// TODO Implement
+	case 1:
+	// TODO Implement
+	case 2:
+	// TODO Implement
+	default:
+		assert (false);
+	}
+}
+
+void
+AccountDataViewModel::GetValue
+(	wxVariant& variant,
+	wxDataViewItem const& item,
+	unsigned int col
+) const
+{
+	// TODO Implement
+}
+
+bool
+AccountDataViewModel::SetValue
+(	wxVariant const& variant,	
+	wxDataViewItem const& item,
+	unsigned int col
+)
+{
+	// TODO Implement
+}
+
+unsigned int
+AccountDataViewModel::get_all_items(wxDataViewItemArray& items) const
+{
+	for
+	(	Map::const_iterator it = m_accounts.begin(), end = m_accounts.end();
+		it != end;
+		++it
+	)
+	{
+		void* identifier = new unsigned int(it->first);
+		items.Add(wxDataViewItem(identifier));
+	}
+	return m_accounts.size();
+}
 
 
 
--- graphical_session/wxs/account_data_view_model.hpp
+++ graphical_session/wxs/account_data_view_model.hpp
@@ -1,7 +1,13 @@
 #ifndef GUARD_account_data_view_model_hpp
 #define GUARD_account_data_view_model_hpp
 
+#include "account.hpp"
+#include <boost/shared_ptr.hpp>
+#include <boost/unordered_map.hpp>
+#include <jewel/decimal.hpp>
 #include <wx/dataview.h>
+#include <wx/variant.h>
+#include <vector>
 
 namespace phatbooks
 {
@@ -11,13 +17,34 @@ namespace gui
 /**
  * This class, by subclassing wxDataViewModel, facilites
  * the presentation of phatbooks::Account-related information
- * in the form of a wxDataViewCtrl.
+ * in the form of a wxDataViewCtrl to be used when editing Accounts.
+ *
+ * There are three columns:\e
+ * 	Account name;\e
+ * 	Account type; and\e
+ * 	Account opening balance.
  */
 class AccountDataViewModel:
 	public wxDataViewModel
 {
 public:
 
+	/**
+	 * We need to hold the Account together with its opening
+	 * balance.
+	 */
+	struct AugmentedAccount
+	{
+		Account account;
+		jewel::Decimal technical_opening_balance;
+	};
+
+	/**
+	 * Construct AccountDataViewModel with a container of the
+	 * Accounts to be modelled.
+	 */
+	AccountDataViewModel(std::vector<AugmentedAccount> const& p_accounts);
+	
 	/**
 	 * Function inherited as pure virtual from wxDataViewModel.
 	 * Indicates whether \e item is a container, i.e. can have
@@ -28,7 +55,7 @@ public:
 	/**
 	 * Function inherited as pure virtual from wxDataViewModel.
 	 *
-	 * @returns the wxDataViewItem represents the parent of item, or
+	 * @returns the wxDataViewItem that represents the parent of item, or
 	 * an invalid wxDataViewItem if the root item is the parent
 	 * item.
 	 */
@@ -38,7 +65,7 @@ public:
 	 * Function inherited as pure virtual from wxDataViewModel.
 	 */
 	virtual unsigned int GetChildren
-	(	wxDataViewItem const&,
+	(	wxDataViewItem const& item,
 		wxDataViewItemArray& children
 	) const;
 
@@ -65,8 +92,30 @@ public:
 		wxDataViewItem const& item,
 		unsigned int col
 	) const;
+
+	/**
+	 * Function inherited as pure virtual from wxDataViewModel.
+	 */
+	virtual bool SetValue
+	(	wxVariant const& variant,
+		wxDataViewItem const& item,
+		unsigned int col
+	);
 	
 private:
+
+	unsigned int get_all_items(wxDataViewItemArray& items) const;
+
+	static unsigned int const s_num_columns = 3;
+
+	unsigned int m_least_available_identifier;
+
+	// Map identifiers to AugmentedAccounts
+	typedef boost::unordered_map
+	<	unsigned int,
+		boost::shared_ptr<AugmentedAccount>
+	> Map;
+	Map m_accounts;
 
 };  // AccountDataViewModel
 
