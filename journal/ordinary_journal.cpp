f0f6a10c53602270238cbd6fa977f4910c7bd4c6
Matthew Harvey <software@matthewharvey.net>
2013-06-21 13:46:42 +0000

    Fixed glitch in output of OrdinaryJournal to stream, whereby a space was missing in "header" line, for journals that do not have an ID.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@1634 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- journal/ordinary_journal.cpp
+++ journal/ordinary_journal.cpp
@@ -224,10 +224,10 @@ OrdinaryJournal::do_output(ostream& os) const
 {
 	os << date() << " ";
 	// lexical_cast here avoids unwanted formatting
-	os << "ORDINARY JOURNAL";
+	os << "ORDINARY JOURNAL ";
 	if (has_id())
 	{
-		os << " ID " << lexical_cast<string>(id()) << " ";
+		os << "ID " << lexical_cast<string>(id()) << " ";
 	}
 	PersistentJournal::do_output(os);
 	return;
