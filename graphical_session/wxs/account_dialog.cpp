7724caa5903a1a659c0fed7b489cdef4754a49a8
Matthew Harvey <software@matthewharvey.net>
2013-06-26 10:12:46 +0000

    Continued working on phatbooks::gui::AccountDialog.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@1671 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- graphical_session/wxs/account_dialog.cpp
+++ graphical_session/wxs/account_dialog.cpp
@@ -12,11 +12,75 @@
 #include <wx/string.h>
 #include <wx/textctrl.h>
 #include <wx/window.h>
+#include <cassert>
 
 namespace phatbooks
 {
 namespace gui
 {
+
+namespace
+{
+	wxString account_super_type_string
+	(	account_super_type::AccountSuperType p_account_super_type
+	)
+	{
+		switch (p_account_super_type)
+		{
+		case account_super_type::balance_sheet:
+			return wxString("Account");
+		case account_super_type::pl:
+			return wxString("Category");
+		default:
+			assert (false);
+		}
+		assert (false);
+	}
+
+	wxString account_name_ctrl_label_string
+	(	account_super_type::AccountSuperType p_account_super_type
+	)
+	{
+		return
+			account_super_type_string(p_account_super_type) +
+			wxString(" name:");
+	}
+
+	wxString account_type_ctrl_label_string
+	(	account_super_type::AccountSuperType p_account_super_type
+	)
+	{
+		return
+			account_super_type_string(p_account_super_type) +
+			wxString(" type:");
+	}
+
+	wxString account_description_label_string
+	(	account_super_type::AccountSuperType p_account_super_type
+	)
+	{
+		(void)p_account_super_type;  // silence compiler re. unused param.
+		return wxString("Description:");
+	}
+
+	wxString opening_amount_label_string
+	(	account_super_type::AccountSuperType p_account_super_type
+	)
+	{
+		switch (p_account_super_type)
+		{
+		case account_super_type::balance_sheet:
+			return wxString("Opening balance:");
+		case account_super_type::pl:
+			return wxString("Initial budget allocation:");
+		default:
+			assert (false);
+		}
+		assert (false);
+	}
+	
+}  // end anonymous namespace
+
 
 AccountDialog::AccountDialog
 (	Frame* p_parent,
@@ -33,8 +97,6 @@ AccountDialog::AccountDialog
 {
 	m_top_sizer = new wxGridBagSizer;
 	SetSizer(m_top_sizer);
-
-
 
 
 
