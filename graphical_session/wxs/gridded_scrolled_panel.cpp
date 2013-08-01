81b2c131a6f4589a500ab1bd664a1878f4d78f4b
Matthew Harvey <software@matthewharvey.net>
2013-08-01 10:25:12 +0000

    Created class GriddedScrolledPanel, which is intended to serve as a base class to factor out various code that is repeated in classes that inherit from wxScrolledWindow and use a wxGridBagSizer as their top sizer.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@1894 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

---
+++ graphical_session/wxs/gridded_scrolled_panel.cpp
@@ -0,0 +1,137 @@
+#include "gridded_scrolled_panel.hpp"
+#include "finformat.hpp"
+#include "locale.hpp"
+#include "phatbooks_database_connection.hpp"
+#include "sizing.hpp"
+#include <jewel/decimal.hpp>
+#include <wx/gbsizer.h>
+#include <wx/gdicmn.h>
+#include <wx/scrolwin.h>
+#include <wx/stattext.h>
+#include <wx/string.h>
+#include <wx/window.h>
+#include <cassert>
+
+
+namespace phatbooks
+{
+namespace gui
+{
+
+GriddedScrolledPanel::GriddedScrolledPanel
+(	wxWindow* p_parent,
+	wxSize const& p_size,
+	PhatbooksDatabaseConnection& p_database_connection,
+	bool p_horizontal_scrolling_enabled
+):
+	wxScrolledWindow
+	(	p_parent,
+		wxID_ANY,
+		wxDefaultPosition,
+		p_size,
+		(p_horizontal_scrolling_enabled? (wxVSCROLL | wxHSCROLL): wxVSCROLL)
+	),
+	m_current_row(0),
+	m_top_sizer(0),
+	m_database_connection(p_database_connection)
+{
+	m_top_sizer = new wxGridBagSizer(standard_gap(), standard_gap());
+	SetSizer(m_top_sizer);
+	int const standard_scrolling_increment = 10;
+	int const horizontal_scrolling_rate =
+		(p_horizontal_scrolling_enabled? standard_scrolling_increment: 0);
+	SetScrollRate(horizontal_scrolling_rate, standard_scrolling_increment);
+}
+
+GriddedScrolledPanel::~GriddedScrolledPanel()
+{
+}
+
+wxGridBagSizer&
+GriddedScrolledPanel::top_sizer()
+{
+	assert (m_top_sizer);
+	return *m_top_sizer;
+}
+
+int
+GriddedScrolledPanel::current_row() const
+{
+	return m_current_row;
+}
+
+void
+GriddedScrolledPanel::increment_row()
+{
+	++m_current_row;
+	return;
+}
+
+void
+GriddedScrolledPanel::decrement_row()
+{
+	--m_current_row;
+}
+
+void
+GriddedScrolledPanel::display_text
+(	wxString const& p_text,
+	int p_column,
+	int p_alignment_flags
+)
+{
+	wxStaticText* text = new wxStaticText
+	(	this,
+		wxID_ANY,
+		p_text,
+		wxDefaultPosition,
+		wxDefaultSize,
+		p_alignment_flags
+	);
+	top_sizer().Add
+	(	text,
+		wxGBPosition(current_row(), p_column),
+		wxDefaultSpan,
+		p_alignment_flags
+	);
+	return;
+}
+
+void
+GriddedScrolledPanel::display_decimal
+(	jewel::Decimal const& p_decimal,
+	int p_column
+)
+{
+	wxStaticText* text = new wxStaticText
+	(	this,
+		wxID_ANY,
+		finformat_wx(p_decimal, locale()),
+		wxDefaultPosition,
+		wxDefaultSize,
+		wxALIGN_RIGHT
+	);
+	top_sizer().Add
+	(	text,
+		wxGBPosition(current_row(), p_column),
+		wxDefaultSpan,
+		wxALIGN_RIGHT
+	);
+	return;
+}
+
+PhatbooksDatabaseConnection&
+GriddedScrolledPanel::database_connection()
+{
+	return m_database_connection;
+}
+
+PhatbooksDatabaseConnection const&
+GriddedScrolledPanel::database_connection() const
+{
+	return m_database_connection;
+}
+
+
+}  // namespace gui
+}  // namespace phatbooks
---
+++ graphical_session/wxs/gridded_scrolled_panel.hpp
@@ -0,0 +1,95 @@
+#ifndef GUARD_gridded_scrolled_panel_hpp
+#define GUARD_gridded_scrolled_panel_hpp
+
+#include <jewel/decimal_fwd.hpp>
+#include <wx/gbsizer.h>
+#include <wx/gdicmn.h>
+#include <wx/scrolwin.h>
+#include <wx/string.h>
+#include <wx/window.h>
+
+namespace phatbooks
+{
+
+// begin forward declarations
+
+class PhatbooksDatabaseConnection;
+
+namespace gui
+{
+
+// end forward declarations
+
+/**
+ * Represents a panel for which the sizing is controlled by a wxGridBagSizer,
+ * for which has scrolling is enabled, and which is associated with a
+ * PhatbooksDatabaseConnection. The class will always have
+ * phatbooks::gui::standard_gap() incorporated into the sizing scheme
+ * (see "sizing.hpp").
+ *
+ * Typically this will be a private or protected base class for more
+ * customized client classes.
+ */
+class GriddedScrolledPanel: wxScrolledWindow
+{
+public:
+
+	/**
+	 * Vertical scrolling is always enabled. Horizontal scrolling
+	 * is only enabled if \e p_horizontal_scrolling_enabled is passed
+	 * \e true.
+	 */
+	GriddedScrolledPanel
+	(	wxWindow* p_parent,
+		wxSize const& p_size,
+		PhatbooksDatabaseConnection& p_database_connection,
+		bool p_horizontal_scrolling_enabled = false
+	);
+
+	virtual ~GriddedScrolledPanel();
+
+	wxGridBagSizer& top_sizer();
+
+	/**
+	 * An instance of GriddedScrolledPanel will keep track of a
+	 * "current row" for purposes of positioning widgets on its sizing
+	 * grid.
+	 */
+	int current_row() const;
+	void increment_row();
+	void decrement_row();
+
+	/**
+	 * Display wxString using a wxStaticText at the position given
+	 * by current_row() and \e p_column.
+	 */
+	void display_text
+	(	wxString const& p_text,
+		int p_column,
+		int p_alignment_flags = wxALIGN_LEFT
+	);
+
+	/**
+	 * Display a jewel::Decimal using a wxStaticText at the position
+	 * given by current_row() and \e p_column.
+	 */
+	void display_decimal
+	(	jewel::Decimal const& p_decimal,
+		int p_column
+	);
+
+	PhatbooksDatabaseConnection& database_connection();
+	PhatbooksDatabaseConnection const& database_connection() const;
+
+private:
+	
+	int m_current_row;
+	wxGridBagSizer* m_top_sizer;
+	PhatbooksDatabaseConnection& m_database_connection;
+
+};  // class GriddedScrolledPanel
+
+}  // namespace gui
+}  // namespace phatbooks
+
+#endif  // GUARD_gridded_scrolled_panel_hpp
