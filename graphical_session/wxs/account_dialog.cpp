4ae81522139a6e4bb9dfa6e50564b5d8e86e46c5
Matthew Harvey <software@matthewharvey.net>
2013-08-29 14:53:55 +0000

    Fixed glitch in AccountDialog where m_visibility_ctrl was always initially checked regardless of actual visibility of Account.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@2034 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- graphical_session/wxs/account_dialog.cpp
+++ graphical_session/wxs/account_dialog.cpp
@@ -366,7 +366,8 @@ AccountDialog::configure_bottom_row()
 		wxSize(medium_width(), m_name_ctrl->GetSize().y),
 		wxALIGN_RIGHT
 	);
-	m_visibility_ctrl->SetValue(true);
+	m_visibility_ctrl->
+		SetValue(m_account.visibility() == visibility::visible);
 	m_top_sizer->Add
 	(	m_visibility_ctrl,
 		wxGBPosition(m_current_row, 2),
