116ee8b85e00f75020cabab719cea5743aa7abe4
Matthew Harvey <software@matthewharvey.net>
2013-09-13 10:23:12 +0000

    Continued working on DateParser.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@2181 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- date/date_parser.cpp
+++ date/date_parser.cpp
@@ -1,29 +1,41 @@
 #include "date_parser.hpp"
+#include <boost/algorithm/string.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/lambda/lambda.hpp>
 #include <boost/optional.hpp>
 #include <boost/unordered_set.hpp>
 #include <jewel/array_utilities.hpp>
+#include <jewel/assert.hpp>
 #include <wx/datetime.h>
 #include <wx/intl.h>
 #include <wx/string.h>
+#include <string>
+#include <vector>
 
+using boost::algorithm::split;
 using boost::optional;
 using boost::unordered_set;
 using jewel::num_elements;
+using std::find;
+using std::string;
+using std::vector;
 
 namespace gregorian = boost::gregorian;
+namespace lambda = boost::lambda;
 
 namespace phatbooks
 {
 
 namespace
 {
-	bool is_format_char(char c)
+	// Returns true if and only if c is one of the special characters that
+	// can appear in a strftime format string representing a field date or
+	// time field.
+	bool is_date_time_format_char(char c)
 	{
 		static char const chars_a[] =
-		{	'a', 'A', 'b', 'h', 'B', 'c', 'C', 'd', 'D', 'F', 'g', 'G', 'H',
-			'I', 'j', 'm', 'M', 'p', 'r', 'R', 'S', 'T', 'u', 'U', 'V', 'w',
-			'W', 'x', 'X', 'y', 'Y', 'z', 'Z'
+		{	'a', 'A', 'b', 'B', 'c', 'D', 'H', 'I', 'j', 'm', 'M', 'P',
+			'S', 'U', 'w', 'W', 'x', 'X', 'y', 'Y', 'Z'
 		};
 		static unordered_set<char> const chars
 		(	jewel::begin(chars_a),
@@ -32,7 +44,102 @@ namespace
 		return chars.find(c) != chars.end();
 	}
 
+	// Returns true if and only if c is one of the special characters that
+	// can appear in a strftime format string representing a field in a tm
+	// struct, which is a date field and not a time field, and which
+	// represents in numerical format, either the day of the month, the
+	// month, or the year.
+	bool is_ordinary_date_format_char(char c)
+	{
+		static char const chars_a[] =
+		{	'D',  // day of the month (01-31)
+			'm',  // month (01-12)
+			'y',  // year in century (00-99)
+			'Y',  // year
+		};
+		char const* const e = jewel::end(chars_a);
+		return find(jewel::begin(chars_a), e, c) != e;
+	}
 
+	// Examines a strftime-like date format string. If the string is an
+	// "ordinary" date format string, returns an optional initialized with the
+	// character that separates the fields in that string; otherwise, returns
+	// an uninitialized optional. For this purpose, the string is an
+	// "ordinary" date format string if and only if: (a) there are exactly
+	// three date fields, being a day-of-month, month, and year field
+	// (not necessarily in that order), such that each of these fields is
+	// represented by a character for which is_ordinary_date_format_char
+	// returns true (along with the usual '%' and possible other modifiers
+	// between the '%' and the format char); and (b) there is exactly one
+	// separator character, which appears twice in the string, separating
+	// the first and second fields, and separating the second and third
+	// fields.
+	optional<char> separating_char(wxString const& str)
+	{
+		// Analyse the string; if at any point we find anything inconsistent
+		// with "ordinary date format", then we return an uninitialized
+		// optional.
+		optional<char> separator;
+		wxString::const_iterator it = str.begin();
+		wxString::const_iterator const end = str.end();
+		size_t num_fields = 0;
+		while (it != end)
+		{
+			if (*it == '%')
+			{
+				// Skip over any "modifiers"
+				while ((it != end) && !is_date_time_format_char(*it)) ++it;
+
+				// If we've reached the end without find a
+				// date_time_format_char, then it's not ordinary format.
+				if (it == end)
+				{
+					return optional<char>();
+				}
+				JEWEL_ASSERT (is_date_time_format_char(*it));
+
+				// If our format char is not ordinary, then it's not
+				// ordinary format.
+				if (!is_ordinary_date_format_char(*it))
+				{
+					return optional<char>();
+				}
+				JEWEL_ASSERT (is_ordinary_date_format_char(*it));
+				++num_fields;
+				++it;
+
+				// Have we found all the fields? Then we should
+				// be at the end.
+				if (num_fields == 3)
+				{
+					JEWEL_ASSERT (separator || (it != end));
+					return separator;
+				}
+				JEWEL_ASSERT (num_fields < 3);
+
+				// To be an ordinary format string,
+				// the next character should be a separator, and not another
+				// format char, and not the end. If we already
+				// have encountered a separator, then this should be an
+				// instance of the same separator.
+				if
+				(	(it == end) ||
+					(is_date_time_format_char(*it)) ||
+					(separator && (*separator != *it))
+				)
+				{
+					return optional<char>();
+				}
+				separator = *it;
+				++it;
+			}
+			else
+			{
+				return optional<char>();
+			}
+		}
+		return separator;
+	}
 
 }  // end anonymous namespace
 
@@ -92,10 +199,32 @@ optional<gregorian::date>
 DateParser::tolerant_parse(wxString const& p_string) const
 {
 	optional<gregorian::date> ret;
-	// TODO HIGH PRIORITY Implement this.
-	
-	
-
+	wxString const* formats[] =
+	{	&m_short_format,
+		&m_long_format
+	};
+	for (size_t i = 0; i != num_elements(formats); ++i)
+	{
+		wxString const& format = *(formats[i]);
+		optional<char> const maybe_sep = separating_char(format);
+		if (!maybe_sep)
+		{
+			continue;
+		}
+		JEWEL_ASSERT (maybe_sep);
+		char const sep = *maybe_sep;
+		vector<wxString> target_fields;
+		vector<wxString> format_fields;
+		split(target_fields, p_string, (lambda::_1 == sep));
+		split(format_fields, p_string, (lambda::_1 == sep));
+		if ((target_fields.size() != 3) || (format_fields.size() != 3))
+		{
+			continue;
+		}
+		JEWEL_ASSERT (target_fields.size() == 3);
+		JEWEL_ASSERT (format_fields.size() == 3);
+		// TODO HIGH PRIORITY...
+	}
 	return ret;
 }
 
