5e620a42f0d9487754cb34be7d300212c93cbb80
Matthew Harvey <software@matthewharvey.net>
2013-11-15 22:17:56 +0000

    Added copyright notice at the top of some non-C++ files that didn't have one before.
    Edited "prebuild_driver.tcl" script so that it reads past the copyright notice
    that now appears at the top of "currencies.csv".
    
    
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/phatbooks/trunk@2703 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- prebuild_driver.tcl
+++ prebuild_driver.tcl
@@ -1,16 +1,50 @@
 #!/usr/bin/env tclsh
 
+###
+# Copyright 2013 Matthew Harvey
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
 # Scans currencies.csv and outputs the repetitive bit of code
 # required for currency.cpp, as file make_currencies_inc.hpp. It is
 # intended make_currencies_inc.hpp be #included in make_currencies.cpp.
 
-set infile [open "currencies.csv" "r"]
+set infile_name "currencies.csv"
+set infile [open $infile_name "r"]
 set outfile [open [file join "include" "make_currencies_inc.hpp"] "w"]
 
-# Discard first line (headers)
+# Discard comments and header row at top of file
+set start_found 0
+while {[gets $infile line] >= 0} {
+    puts $line
+    if {[string equal $line "START"]} {
+        set start_found 1
+        break
+    }
+}
+if {!$start_found} {
+    puts "Could not find \"START\" row while reading ${infile_name}."
+    exit 1
+}
 gets $infile line
+if {![string equal $line "Currency,Symbol,Precision"]} {
+    puts "Could not find header row while reading ${infile_name}."
+    exit 1
+}
 
-while {[gets $infile line] > 0} {
+# Read content
+while {[gets $infile line] >= 0} {
 	if {$line == {END}} {
 		break
 	}
