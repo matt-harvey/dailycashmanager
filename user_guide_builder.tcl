8a24d86d0d180b4cddf4cc22fb7675e9fede66f9
Matthew Harvey <software@matthewharvey.net>
2014-01-10 22:18:17 +0000

    Wrote Tcl script to automate building of user guide.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/dcm/trunk@2854 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

---
+++ user_guide_builder.tcl
@@ -0,0 +1,34 @@
+#!/usr/bin/env tclsh
+
+###
+# Copyright 2013 Matthew Harvey
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+# Copy text of LICENSE into "user_guide" directory, with suitable title and
+# file extension to satisfy Sphinx.
+set in [open "LICENSE" r]
+set out [open "user_guide/LICENSE.rst" w]
+puts $out "LICENSE"
+puts $out "======="
+puts $out ""
+while {[gets $in line] >= 0} {
+    puts $out $line
+}
+close $in
+flush $out
+close $out
+
+# Generate html documentation using Sphinx
+exec sphinx-build -b html -d user_guide/_build/doctrees user_guide user_guide/html
