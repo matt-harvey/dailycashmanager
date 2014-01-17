ab9cc83acf731699cf28414df06971638062685e
Matthew Harvey <software@matthewharvey.net>
2014-01-17 04:59:35 +0000

    Created build target "desktop_file", whereby, on Linux, "make desktop_file" invokes a script "generate_desktop_file.tcl" which creates a "dailycashmanager.desktop" file, which can be used for configuring icon etc. on Linux.
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/dcm/trunk@2873 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

---
+++ generate_desktop_file.tcl
@@ -0,0 +1,41 @@
+#!/usr/bin/env tclsh
+
+###
+# Copyright 2014 Matthew Harvey
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+# Generates the ".desktop" file for Linux builds.
+
+set req_args 3
+if {$argc != $req_args} {
+    error "Wrong number of arguments: should be $req_args."
+}
+set application_name [lindex $argv 0]
+set executable_name [lindex $argv 1]
+set path_to_icon [lindex $argv 2]
+set out [open "${executable_name}.desktop" w]
+puts $out {[Desktop Entry]}
+puts $out ""
+puts $out "\tValue=1.0"
+puts $out "\tType=Application"
+puts $out "\tName=${application_name}"
+puts $out "\tGenericName=\"Personal budgeting application\""
+puts $out "\tEncoding=UTF-8"
+puts $out ""
+puts $out "\tExec=${executable_name} %f"
+puts $out "\tIcon=${path_to_icon}"
+puts $out ""
+flush $out
+close $out
