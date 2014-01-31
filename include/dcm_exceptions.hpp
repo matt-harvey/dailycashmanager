a7fb4e5f8238027de491bc34566d9bc5eb33e0f0
Matthew Harvey <software@matthewharvey.net>
2014-01-31 11:08:29 +1030

    Fixed API documentation for the file dcm_exceptions.hpp.

--- include/dcm_exceptions.hpp
+++ include/dcm_exceptions.hpp
@@ -17,8 +17,6 @@
 #ifndef GUARD_dcm_exceptions_hpp_9435394075436235
 #define GUARD_dcm_exceptions_hpp_9435394075436235
 
-#include <jewel/exception.hpp>
-
 /** @file
  *
  * @brief Header file containing exception classes defined in the dcm
@@ -27,20 +25,28 @@
  * these exceptions).
  *
  * These exceptions are defined using the JEWEL_DERIVED_EXCEPTION macro from
- * the Jewel library. They all have \c jewel::Exception as a (direct or
- * indirect) base class.
+ * the Jewel library. They all have jewel::Exception as a (direct or
+ * indirect) base class, which is defined in <jewel/exception.hpp>. Briefly,
+ * a statement of the form "JEWEL_DERIVED_EXCEPTION(D, B)" declares and defines
+ * an exception class D that publicly inherits from the class B.
+ *
+ * @include dcm_exceptions.hpp
  */
+
+/// @cond
+
+#include <jewel/exception.hpp>
 
 namespace dcm
 {
 
-/**
+/*
  * Base class for all the exception classes defined in the \c dcm
  * namespace.
  */
 JEWEL_DERIVED_EXCEPTION(DcmException, jewel::Exception);
 
-/**
+/*
  * Exception to be thrown when program attempts to execute code which
  * depends on some functionality which has not yet been implemented.
  * This exception should not feature in release builds.
@@ -50,25 +56,25 @@ JEWEL_DERIVED_EXCEPTION
 	jewel::Exception
 );
 
-/**
+/*
  * Exception to be thrown when an object has not met certain preconditions
  * for being stored in the database.
  */
 JEWEL_DERIVED_EXCEPTION(StoragePreconditionsException, DcmException);
 
-/**
+/*
  * Exception to be thrown when an invalid conversion is attempted between
  * one date format and another.
  */
 JEWEL_DERIVED_EXCEPTION(DateConversionException, DcmException);
 
-/**
+/*
  * Exception to be thrown when an unsafe arithmetic operation (addition,
  * subtraction etc.) would be performed.
  */
 JEWEL_DERIVED_EXCEPTION(UnsafeArithmeticException, DcmException);
 
-/**
+/*
  * Exception to be thrown when a numerical amount (especially, but not
  * necessarily, a jewel::Decimal) has a precision which is an invalid
  * precision in some given context.
@@ -80,26 +86,26 @@ JEWEL_DERIVED_EXCEPTION(PrecisionException, DcmException);
  */
 JEWEL_DERIVED_EXCEPTION(InvalidJournalException, DcmException);
 
-/**
+/*
  * Exception to be thrown when an action is attempted on a Journal (or
  * OrdinaryJournal or DraftJournal) which requires that Journal to be
  * in a balanced state, and that Journal is not in a balanced state.
  */
 JEWEL_DERIVED_EXCEPTION(UnbalancedJournalException, InvalidJournalException);
 
-/**
+/*
  * Exception to be thrown when a PersistentJournal is such that posting it
  * would cause overflow or unacceptable precision loss in Account balances.
  */
 JEWEL_DERIVED_EXCEPTION(JournalOverflowException, InvalidJournalException);
 
-/**
+/*
  * Exception to be thrown when a Frequency is not supported or valid
  * in some context.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidFrequencyException, DcmException);
 
-/**
+/*
  * Exception to be thrown when an attempt is made to get the
  * balancing account from AmalgamatedBudget when it has not
  * been initialized.
@@ -109,18 +115,18 @@ JEWEL_DERIVED_EXCEPTION
 	DcmException
 );
 
-/**
+/*
  * Exception to be thrown when an attempt is made to edit budgets
  * in an invalid way.
  */
 JEWEL_DERIVED_EXCEPTION(BudgetEditingException, DcmException);
 
-/**
+/*
  * Exception to be thrown when a BudgetItem is invalid in some context.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidBudgetItemException, DcmException);
 
-/**
+/*
  * Exception to be thrown when there is an attempt made to delete the
  * persistent database record of a sqloxx::PersistentObject (or derived
  * class e.g. dcm::Journal)
@@ -128,31 +134,31 @@ JEWEL_DERIVED_EXCEPTION(InvalidBudgetItemException, DcmException);
  */
 JEWEL_DERIVED_EXCEPTION(PreservedRecordDeletionException, DcmException);
 
-/**
+/*
  * Exception to be thrown if an attempt is made to set the date of an
  * OrdinaryJournal to a prohibited date.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidJournalDateException, DcmException);
 
-/**
+/*
  * Exception to be thrown if an attempt is made to set the next_date
  * of a Repeater to a prohibited date.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidRepeaterDateException, DcmException);
 
-/**
+/*
  * Exception to be thrown when there is an attempt to do something
  * "invalid" with the entity creation date of an accounting entity.
  */
 JEWEL_DERIVED_EXCEPTION(EntityCreationDateException, DcmException);
 
-/**
+/*
  * Exception to be thrown when something purporting to be the name of
  * an Account is not in fact the name of an Account.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidAccountNameException, DcmException);
 
-/**
+/*
  * Exception to be thrown when an Account or a range of Accounts
  * is invalid in some context.
  */
@@ -164,26 +170,26 @@ JEWEL_DERIVED_EXCEPTION(InvalidAccountException, DcmException);
  */
 JEWEL_DERIVED_EXCEPTION(DuplicateAccountNameException, DcmException);
 
-/**
+/*
  * Exception to be thrown when something is about a Commodity puts it
  * in a state whereby it cannot validly be the default Commodity for the
  * entity represented by a DcmDatabaseConnection.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidDefaultCommodityException, DcmException);
 
-/**
+/*
  * Exception be thrown by CurrencyManager when exceptional behaviour occurs
  * that is specifically related to that class.
  */
 JEWEL_DERIVED_EXCEPTION(CurrencyManagerException, DcmException);
 
-/**
+/*
  * Exception to be thrown by DefaultAccountGenerator when exceptional
  * behaviour occurs that is specifically related to that class.
  */
 JEWEL_DERIVED_EXCEPTION(DefaultAccountGeneratorException, DcmException);
 
-/**
+/*
  * Exception to be thrown when a string does not correspond to any
  * AccountType, but purports to do so.
  */
@@ -192,31 +198,31 @@ JEWEL_DERIVED_EXCEPTION
 	DcmException
 );
 
-/**
+/*
  * Exception to be thrown when an AccountType or a range of AccountTypes
  * is invalid in a given context.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidAccountTypeException, DcmException);
 
-/**
+/*
  * Exception to be thrown by AccountDataViewModel when exceptional behaviour
  * occurs that is specifically related to that class.
  */
 JEWEL_DERIVED_EXCEPTION(AccountDataViewModelException, DcmException);
 
-/**
+/*
  * Exception to be thrown when something that is supposed to represent a
  * date, does not in fact represent a valid date.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidDateException, DcmException);
 
-/**
+/*
  * Exception to be thrown when something that is supposed to represent a
  * TransactionType, does not in fact represent a TransactionType.
  */
 JEWEL_DERIVED_EXCEPTION(InvalidTransactionTypeException, DcmException);
 
-/**
+/*
  * Exception to be thrown when a unique name cannot be found for some operation
  * which requires a unique name to be generated in order to succeed.
  */
@@ -224,5 +230,6 @@ JEWEL_DERIVED_EXCEPTION(UniqueNameException, DcmException);
 
 }  // namespace dcm
 
-#endif  // GUARD_dcm_exceptions_hpp_9435394075436235
+/// @endcond
 
+#endif  // GUARD_dcm_exceptions_hpp_9435394075436235
